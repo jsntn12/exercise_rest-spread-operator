644cf267eb869b979b54bde8dd7fadfc4edd3e87
jsntn12 <jsantana@nperts-mbp.mynetworksettings.com>
2023-03-26 17:52:08 -0400

    fix: formatted document using prettier

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 // }
 // Refactor it to use the rest operator & an arrow function:
 /* Write an ES2015 Version */
-const filterOutOdds = (...nums) => nums.filter(n => v % 2 === 0)
+const filterOutOdds = (...nums) => nums.filter((n) => v % 2 === 0);
 
 // findMin
 // Write a function called findMin that accepts a variable number of arguments and returns the smallest argument.
@@ -18,23 +18,21 @@ const filterOutOdds = (...nums) => nums.filter(n => v % 2 === 0)
 // findMin(1,4,12,-3) // -3
 // findMin(1,-1) // -1
 // findMin(3,1) // 1
-const findMin = (...nums) => Math.min(...nums)
-
-
+const findMin = (...nums) => Math.min(...nums);
 
 // mergeObjects
 // Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
 // mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
-const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2})
-
+const mergeObjects = (obj1, obj2) => ({ ...obj1, ...obj2 });
 
 // doubleAndReturnArgs
 // Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
 // doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
 // doubleAndReturnArgs([2],10,4) // [2, 20, 8]
-const doubleAndReturnArgs = (arr, ...nums) => [...arr, ...doubleAndReturnArgs.map(num => num * 2)]
-
-
+const doubleAndReturnArgs = (arr, ...nums) => [
+	...arr,
+	...doubleAndReturnArgs.map((num) => num * 2),
+];
 
 // Slice and Dice!
 // For this section, write the following functions using rest, spread and refactor these functions to be arrow functions!
@@ -45,47 +43,43 @@ const doubleAndReturnArgs = (arr, ...nums) => [...arr, ...doubleAndReturnArgs.ma
 and return a new array without that item. */
 
 const removeRandom = (items) => {
-    const indexRemove = Math.floor(Math.random() * items.length);
-    return [...items.slice(0, indexRemove), ...items.slice(indexRemove + 1)]    
-}
-
+	const indexRemove = Math.floor(Math.random() * items.length);
+	return [...items.slice(0, indexRemove), ...items.slice(indexRemove + 1)];
+};
 
 /** Return a new array with every item in array1 and array2. */
 
-const extend = (array1, array2) => { 
-    return [...array1, ...array2]};
+const extend = (array1, array2) => {
+	return [...array1, ...array2];
+};
 
 /** Return a new object with all the keys and values
 from obj and a new key/value pair */
 
 const addKeyVal = (obj, key, val) => {
-    const newObj = {...obj};
-    newObj[key]= val;
-    return newObj
-}
-
+	const newObj = { ...obj };
+	newObj[key] = val;
+	return newObj;
+};
 
 /** Return a new object with a key removed. */
 
 const removeKey = (obj, key) => {
-    const newObj = {...obj};
-    delete newObj[key];
-    return newObj
-}
-
+	const newObj = { ...obj };
+	delete newObj[key];
+	return newObj;
+};
 
 /** Combine two objects and return a new object. */
 
 const combine = (obj1, obj2) => {
-    return {...obj1, ...obj2}
-}
-
+	return { ...obj1, ...obj2 };
+};
 
 /** Return a new object with a modified key and value. */
 
-const update = (obj,key,val) => {
-    const newObj = {...obj};
-    newObj[key] = val;
-    return newObj
-}
-
+const update = (obj, key, val) => {
+	const newObj = { ...obj };
+	newObj[key] = val;
+	return newObj;
+};
